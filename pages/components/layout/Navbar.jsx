3333e71f8738a7b003744d3d1ccf7e1a3cb219ea
Austin <austinmfarwell@gmail.com>
2022-03-28 14:07:20 -0700

    merge by austin

--- pages/components/layout/Navbar.jsx
+++ pages/components/layout/Navbar.jsx
@@ -3,11 +3,15 @@ import Image from 'next/image'
 
 
 const Navbar = () => {
+
   const [loggedIn, setLoggedIn] = useState(false)
   return (
     <nav>
+      <div className="image-container">
       <Image className='logo' src={'/newLogo.png'} alt="logo" height='56.25' width='112.5'/>
-      <div className="nav-list">
+      <h1>Hairstyling</h1>
+      </div>
+      <div className="nav-link">
         <h3>{loggedIn ? "Logout" : 'Login'}</h3>
       </div>
     </nav>
