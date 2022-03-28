7a59e4f1c2c518d21a8dbe0344aa1c5c52bc44c1
Austin <austinmfarwell@gmail.com>
2022-03-28 15:27:58 -0700

    merge by austin, started styling for home, not finished

---
+++ components/layout/UpcomingClients.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UpcomingClients = () => {
+  return (
+    <div>UpcomingClients</div>
+  )
+}
+
+export default UpcomingClients
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,48 +1,65 @@
-import React, {useState} from 'react'
-import {Grid, Form, Divider} from "semantic-ui-react"
+import React, { useState } from 'react'
+import { Grid, Form, Divider } from "semantic-ui-react"
 import Head from 'next/head'
 import Image from 'next/image'
-import Navbar from './components/layout/Navbar'
+import Navbar from '../components/layout/Navbar'
+// import NewVisitForm from '../components/layout/NewVisitForm'
+// import NewClientForm from '../components/layout/NewClientForm'
+import ActiveClients from '../components/layout/AcitveClients'
+import UpcomingClients from '../components/layout/UpcomingClients'
 
 
-const index = ({name}) => {
+const index = ({ name }) => {
+  //! What is name? 
+  //? Is that the for the pin for clocking in?
 
-    //* UseStates//
-    const [media, setMedia] = useState(null);
-    const [mediaPreview, setMediaPreview] = useState(null);
+  //* UseStates//
+  const [media, setMedia] = useState(null);
+  const [mediaPreview, setMediaPreview] = useState(null);
 
-    //* handlers //
-    const handleChange = (e) => {
-        const { name, value, files } = e.target;
-    
-        if(name === "media" && files.length){
-          setMedia(files[0])
-          setMediaPreview(() => URL.createObjectURL(files[0]))
-        } else {
-          setUser((prev) => ({ ...prev, [name]: value }));
-        }
-    
-      };
+  //* handlers //
+  const handleChange = (e) => {
+    const { name, value, files } = e.target;
+
+    if (name === "media" && files.length) {
+      setMedia(files[0])
+      setMediaPreview(() => URL.createObjectURL(files[0]))
+    } else {
+      setUser((prev) => ({ ...prev, [name]: value }));
+    }
+
+  };
 
 
   return (
     <>
-    <Navbar/>
-    <Grid>
-        <Grid.Column>
-            <Form>
-                <Form.Input 
-                label="Name"
-                required
-                placeholder="Name"
-                value={name}
-                onChange={handleChange}
-                icon="user"
-                iconPosition="left"
-                />
-            </Form>
-        </Grid.Column>
-    </Grid>
+      <Navbar />
+      <main className="main-container">
+
+        {/* This needs to have an onClick function that renders the dropdown  */}
+        <div className="radial-buttons">
+          <div className="new-visit">
+            <h1>New Visit</h1>
+          </div>
+
+          {/* This needs to have an onClick to render the form for new clients ~~~~ This is if they aren't in the system from a previous visit*/}
+          <div className="new-client">
+            <h1>New Client</h1>
+          </div>
+        </div>
+
+
+        <div className="client-lists">
+          <div className="active-clients">
+            <ActiveClients />
+          </div>
+
+          <div className="upcoming-clients">
+            <UpcomingClients />
+          </div>
+        </div>
+
+      </main>
     </>
   )
 }
