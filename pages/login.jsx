3efdbba8bd56ec1e743e48356a56ac0b660d282c
David M <davidm8624@gmail.com>
2022-03-28 14:51:17 -0700

    page holders

---
+++ pages/login.jsx
@@ -0,0 +1,15 @@
+import React, {useState} from 'react'
+import {Grid, Form, Divider} from "semantic-ui-react"
+import Head from 'next/head'
+import Image from 'next/image'
+import Navbar from './components/layout/Navbar'
+
+
+const login = () => {
+  return (<>
+    <Navbar/>
+    </>
+  )
+}
+
+export default login
