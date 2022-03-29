00af09ef0cd99dbcaa41e4187fbf2970c4d66902
David M <davidm8624@gmail.com>
2022-03-29 15:19:01 -0700

    now

--- pages/profile.jsx
+++ pages/profile.jsx
@@ -1,7 +1,5 @@
 // this will be to display the students profile only
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 import React, {useState} from 'react'
 import {Grid, Form, Divider} from "semantic-ui-react"
 import Head from 'next/head'
@@ -12,30 +10,6 @@ const personalProfile = () => {
   const [isTeacher, setIsTeacher] = useState(false)
   return (<>
     <Navbar/>
-=======
-import React, { useState } from "react";
-import { Grid, Form, Divider } from "semantic-ui-react";
-import Head from "next/head";
-import Image from "next/image";
-import Navbar from "../components/layout/Navbar";
-
-const personalProfile = () => {
-  return (
-    <>
-      <Navbar />
->>>>>>> Stashed changes
-=======
-import React, { useState } from "react";
-import { Grid, Form, Divider } from "semantic-ui-react";
-import Head from "next/head";
-import Image from "next/image";
-import Navbar from "../components/layout/Navbar";
-
-const personalProfile = () => {
-  return (
-    <>
-      <Navbar />
->>>>>>> Stashed changes
     </>
   );
 };
