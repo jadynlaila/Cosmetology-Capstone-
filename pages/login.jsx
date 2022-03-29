c42536ba1a35eae8cf531c76d2761848919d94f1
Liam <shadowteckst@gmail.com>
2022-03-29 13:36:49 -0700

    reworked client model and fixed error on login.jsx

--- pages/login.jsx
+++ pages/login.jsx
@@ -2,7 +2,7 @@ import React, {useState} from 'react'
 import {Grid, Form, Divider} from "semantic-ui-react"
 import Head from 'next/head'
 import Image from 'next/image'
-import Navbar from './components/layout/Navbar'
+import Navbar from '../components/layout/Navbar'
 
 
 const login = () => {
