8dbc03e83e34b9803fd038587e176f6740dfbf13
Austin <austinmfarwell@gmail.com>
2022-03-29 15:13:43 -0700

    merge by austin, css needs fix

--- components/layout/AcitveClients.jsx
+++ components/layout/AcitveClients.jsx
@@ -4,7 +4,25 @@ const AcitveClients = () => {
   return (
     <>
     <div className='header'>Acitve Clients</div>
-    <div className="content">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore possimus ab, tempora natus iusto in accusamus, qui sequi quos temporibus dolor? Ullam itaque quo cupiditate dignissimos, quidem quisquam labore deleniti!</div>
+    <div className="content">
+      <div className="name">Pretend Name</div>
+      <div className="name">Pretend Name</div>
+      <div className="name">Pretend Name</div>
+      <div className="name">Pretend Name</div>
+      <div className="name">Pretend Name</div>
+      <div className="name">Pretend Name</div>
+      <div className="name">Pretend Name</div>
+      <div className="name">Pretend Name</div>
+      <div className="name">Pretend Name</div>
+      <div className="name">Pretend Name</div>
+      <div className="name">Pretend Name</div>
+      <div className="name">Pretend Name</div>
+      <div className="name">Pretend Name</div>
+      <div className="name">Pretend Name</div>
+      <div className="name">Pretend Name</div>
+      <div className="name">Pretend Name</div>
+    </div>
+    
     </>
 
   )
