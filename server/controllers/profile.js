7d02b9f2986e97dd9616cad65ba698cd3d2a74e8
Fernando Rodriguez Agredano <Frodri300@west-mec.local>
2022-03-29 18:23:48 -0700

    Controllers

--- server/controllers/profile.js
+++ server/controllers/profile.js
@@ -7,14 +7,47 @@ const VisitModel = require("../models/VisitModel");
 GET PROFILE
 .put('/profile/:id')
 req.params {id}
+
+//! Two seperate getProfile as there are two differnt Models
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-const getProfile = async (req, res) => {
-  const { id } = req.params;
+const getProfileStylist = async (req, res) => {
   try {
-    return res.status(200).json();
+    const stylist = await StylistModel.findOne({stylist: stylist._id}).populate("stylist")
+    if(!stylist) return res.status(404).send("Stylist not Found :|")
+
+    const {
+      name,
+      clients,
+      teacher,
+      email,
+      profilePicURL,
+      s1hours,
+      s2hours,
+      s3hours,
+      s4hours,
+    } = stylist
+
+    return res.status(200).json(stylist);
   } catch (error) {
     console.log(error);
-    return res.status(500).send("Server Error @ updateStylist");
+    return res.status(500).send("Server Error @ getProfileStylist");
+  }
+};
+const getProfileTeacher = async (req, res) => {
+  try {
+    const teacher = await TeacherModel.findOne({teacher: teacher._id}).populate("teacher")
+    if(!teacher) return res.status(404).send("Teacher not Found :|")
+
+    const {
+      students,
+      name,
+      email,
+    } = teacher
+
+    return res.status(200).json(teacher);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send("Server Error @ getPRofileTeacher");
   }
 };
 
@@ -68,145 +101,7 @@ const updateTeacher = async (req, res) => {
   }
 };
 
-// const getProfile = async (req, res) => {
-//   try {
-//     const { username } = req.params;
 
-//     const user = await UserModel.findOne({ username: username.toLowerCase() });
-//     if (!user) {
-//       return res.status(404).send("No User Found");
-//     }
-
-//     const profile = await ProfileModel.findOne({ user: user._id }).populate(
-//       "user"
-//     );
-
-//     const profileFollowStats = await FollowerModel.findOne({ user: user._id });
-
-//     return res.status(200).json({
-//       profile,
-//       followersLength:
-//         profileFollowStats.followers.length > 0
-//           ? profileFollowStats.followers.length
-//           : 0,
-//       followingLength:
-//         profileFollowStats.following.length > 0
-//           ? profileFollowStats.following.length
-//           : 0,
-//     });
-//   } catch (error) {
-//     console.log(error);
-//     return res.status(500).send("Error at getProfile");
-//   }
-// };
-const getUserPosts = async (req, res) => {
-  try {
-    const { username } = req.params;
-    const user = await UserModel.findOne({ username: username.toLowerCase() });
-
-    if (!user) {
-      return res.status(404).send("User Not Found");
-    }
-
-    const posts = await PostModel.find({ user: user._id })
-      .sort({ createdAt: -1 })
-      .populate("user")
-      .populate("comments.user");
-
-    return res.status(200).json(posts);
-  } catch (error) {
-    console.log(error);
-    return res.status(500).send("Error at getUserPosts");
-  }
-};
-
-const getFollowers = async (req, res) => {
-  try {
-    const { userId } = req.params;
-    const user = await FollowerModel.findOne({ user: userId }).populate(
-      "followers.user"
-    );
-    return res.status(200).json(user.followers);
-  } catch (error) {
-    console.log(error);
-    return res.status(500).send("Error at getFollowers");
-  }
-};
-const getFollowing = async (req, res) => {
-  try {
-    const { userId } = req.params;
-    const user = await FollowerModel.findOne({ user: userId }).populate(
-      "following.user"
-    );
-    return res.status(200).json(user.following);
-  } catch (error) {
-    console.log(error);
-    return res.status(500).send("Error at getFollowing");
-  }
-};
-const followUser = async (req, res) => {
-  try {
-    const { userId } = req;
-    const { userToFollowId } = req.params;
-
-    const user = await FollowerModel.findOne({ user: userId });
-    const userToFollow = await FollowerModel.findOne({ user: userToFollowId });
-
-    if (!user || !userToFollow) return res.status(404).send("user not found");
-
-    const isFollowing = user.following.find(
-      (eachUser) => eachUser.user._id.toString() === userToFollowId
-    );
-
-    if (isFollowing) return res.status(401).send("user already followed");
-
-    await user.following.unshift({ user: userToFollowId });
-    await userToFollow.followers.unshift({ user: userId });
-
-    await user.save();
-    await userToFollow.save();
-
-    return res.status(200).send("User Followed");
-  } catch (error) {
-    console.log(error);
-    return res.status(500).send("Error at followUser");
-  }
-};
-const unfollowUser = async (req, res) => {
-  try {
-    const { userId } = req;
-    const { userToUnfollowId } = req.params;
-
-    const user = await FollowerModel.findOne({ user: userId });
-    const userToUnfollow = await FollowerModel.findOne({
-      user: userToUnfollowId,
-    });
-
-    if (!user || !userToUnfollow) return res.status(404).send("user not found");
-
-    const isFollowingIndex = user.following.findIndex(
-      (eachUser) => eachUser.user._id.toString() === userToUnfollowId
-    );
-
-    if (isFollowingIndex === -1)
-      return res.status(401).send("user not followed before");
-
-    await user.following.splice(isFollowingIndex, 1);
-    await user.save();
-
-    const removeFollowerIndex = userToUnfollow.followers.findIndex(
-      (eachUser) => eachUser.user._id.toString() === userId
-    );
-
-    await userToUnfollow.followers.splice(removeFollowerIndex, 1);
-    await userToUnfollow.save();
-
-    return res.status(200).send(`User Unfollowed`);
-  } catch (error) {
-    console.log(error);
-    return res.status(500).send("Error at unfollowUser");
-  }
-};
 const updateProfile = async (req, res) => {
   try {
   } catch (error) {
@@ -221,3 +116,6 @@ const updatePassword = async (req, res) => {
     return res.status(500).send("Error at updatePassword");
   }
 };
+
+
+module.exports = {getProfileStylist, getProfileTeacher}
--- server/routes/postUserRoutes.js
+++ server/routes/postUserRoutes.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const { createStylist, createClient, createTeacher } = require('../controllers/users');
 
 router.route('/stylist').post(createStylist);
-router.route('/client').post(createClient);
+router.route('/').post(createClient);
 router.route('/teacher').post(createTeacher)
 
 module.exports = router;
