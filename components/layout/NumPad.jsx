27bd2e08a1e8481c16bef44403b620a718e520c6
Austin <austinmfarwell@gmail.com>
2022-04-04 15:27:15 -0700

    merge by austin

--- components/layout/NumPad.jsx
+++ components/layout/NumPad.jsx
@@ -1,25 +1,25 @@
 import React from "react";
 
-const NumPad = ({ setPinInput}) => {
+const NumPad = ({ setPinInput }) => {
   return (
     <>
-      
-    <div className="container">
-    <div className="numpad">
-      <div class="button" onClick={() => setPinInput((prev) => prev + '1')} accessKey="1">1</div>
-      <div class="button" onClick={() => setPinInput((prev) => prev + '2')} accessKey="2">2</div>
-      <div class="button" onClick={() => setPinInput((prev) => prev + '3')} accessKey="3">3</div>
-      <div class="button" onClick={() => setPinInput((prev) => prev + '4')} accessKey="4">4</div>
-      <div class="button" onClick={() => setPinInput((prev) => prev + '5')} accessKey="5">5</div> 
-      <div class="button" onClick={() => setPinInput((prev) => prev + '6')} accessKey="6">6</div>
-      <div class="button" onClick={() => setPinInput((prev) => prev + '7')} accessKey="7">7</div>
-      <div class="button" onClick={() => setPinInput((prev) => prev + '8')} accessKey="8">8</div>
-      <div class="button" onClick={() => setPinInput((prev) => prev + '9')} accessKey="9">9</div>
-      <div class="button" accessKey=""></div>
-      <div class="button" onClick={() => setPinInput((prev) => prev + '0')} accessKey="0">0</div>
-      <div class="button" onClick={() => setPinInput((prev) => prev.substring(0, prev.length-1))}><i class="angle left icon"></i></div>
-    </div>
-    </div>
+
+      <div className="container">
+        <div className="numpad">
+          <div class="button" onClick={() => setPinInput((prev) => prev + '1')} accessKey="1">1</div>
+          <div class="button" onClick={() => setPinInput((prev) => prev + '2')} accessKey="2">2</div>
+          <div class="button" onClick={() => setPinInput((prev) => prev + '3')} accessKey="3">3</div>
+          <div class="button" onClick={() => setPinInput((prev) => prev + '4')} accessKey="4">4</div>
+          <div class="button" onClick={() => setPinInput((prev) => prev + '5')} accessKey="5">5</div>
+          <div class="button" onClick={() => setPinInput((prev) => prev + '6')} accessKey="6">6</div>
+          <div class="button" onClick={() => setPinInput((prev) => prev + '7')} accessKey="7">7</div>
+          <div class="button" onClick={() => setPinInput((prev) => prev + '8')} accessKey="8">8</div>
+          <div class="button" onClick={() => setPinInput((prev) => prev + '9')} accessKey="9">9</div>
+          <div id="disapear" class="button disapear" ></div>
+          <div class="button" onClick={() => setPinInput((prev) => prev + '0')} accessKey="0">0</div>
+          <div id="disapear" class="button disapear" ></div>
+        </div>
+      </div>
     </>
   );
 };
--- pages/login.jsx
+++ pages/login.jsx
@@ -12,9 +12,21 @@ const login = () => {
       <Navbar />
       {/* <Form>
       </Form> */}
-      <NumPad pinInput={pinInput} setPinInput={setPinInput}/>
-      <input type="text" value={pinInput} />
-      
+      <div id="input-field">
+        <div class="confirmation-dots">
+          <svg>
+            <g>
+              <circle class="pin-circle" cx="10" cy="10" r="8"></circle>
+              <circle class="pin-circle" cx="50" cy="10" r="8"></circle>
+              <circle class="pin-circle" cx="90" cy="10" r="8"></circle>
+              <circle class="pin-circle" cx="130" cy="10" r="8"></circle>
+            </g>
+          </svg>
+        </div>
+        <input className="dottedPassword" type="password" value={pinInput} />
+        <div id="button-backspace" onClick={() => setPinInput((prev) => prev.substring(0, prev.length - 1))}><i class="angle left icon"></i></div>
+      </div>
+      <NumPad pinInput={pinInput} setPinInput={setPinInput} />
     </>
   );
 };
