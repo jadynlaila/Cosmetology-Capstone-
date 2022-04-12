9a0b03d074b7a703a4ce1438040a176ea7a8b80a
Austin <austinmfarwell@gmail.com>
2022-04-12 15:27:00 -0700

    merge by austin

--- components/layout/Signup.jsx
+++ components/layout/Signup.jsx
@@ -1,11 +1,55 @@
-import React from "react";
-import { Input, UI, Fluid, Icon, Container } from "semantic-ui-react";
+import React, {useState} from "react";
+import { Input, UI, Fluid, Icon, Container, Header, Form, Label, Divider, Button} from "semantic-ui-react";
+import TeacherDropdown from "../Signup/Dropdown";
 
 const Signup = () => {
+  const [teacherSignedIn, setTeacherSignedIn] = useState(true)
   return <>
-  <div id="sign">
-    
-  </div>
+  <div className="signupPage">
+        <Header>Signup</Header>
+        {/* FORM FIELD */}
+        <div className="form-container">
+          {teacherSignedIn ?
+            <>
+              <Label>
+                <h3>
+                  Who is your Teacher?
+                </h3>
+              </Label>
+              <TeacherDropdown />
+            </>
+            :
+            <Form>
+              <Form.Field>
+                <label>Email:</label>
+                <input placeholder="Email" />
+              </Form.Field>
+              <Form.Field>
+                <label>Password:</label>
+                <input placeholder="Password" />
+              </Form.Field>
+              <Form.Field>
+                <label>Phone Number:</label>
+                <input placeholder="Phone Number..." />
+              </Form.Field>
+              <Form.Field>
+                <label>DOB:</label>
+                <input placeholder="DOB..." />
+              </Form.Field>
+            </Form>
+          }
+        </div>
+        <Divider />
+        <footer>
+          <Button
+            content="Next"
+            labelPosition="right"
+            icon="arrow right"
+            // onClick={() => setOpen(false)}
+            positive
+          />
+        </footer>
+      </div>
   </>;
 };
 
--- pages/signup.jsx
+++ pages/signup.jsx
@@ -11,55 +11,12 @@ import TeacherDropdown from "../components/Signup/Dropdown";
 
 
 const signup = () => {
-  const [teacherSignedIn, setTeacherSignedIn] = useState(true)
+  
   return (
     <>
       <Navbar />
-
-      <div className="signupPage">
-        <Header>Signup</Header>
-        {/* FORM FIELD */}
-        <div className="form-container">
-          {teacherSignedIn ?
-            <>
-              <Label>
-                <h3>
-                  Who is your Teacher?
-                </h3>
-              </Label>
-              <TeacherDropdown />
-            </>
-            :
-            <Form>
-              <Form.Field>
-                <label>Email:</label>
-                <input placeholder="Email" />
-              </Form.Field>
-              <Form.Field>
-                <label>Password:</label>
-                <input placeholder="Password" />
-              </Form.Field>
-              <Form.Field>
-                <label>Phone Number:</label>
-                <input placeholder="Phone Number..." />
-              </Form.Field>
-              <Form.Field>
-                <label>DOB:</label>
-                <input placeholder="DOB..." />
-              </Form.Field>
-            </Form>
-          }
-        </div>
-        <Divider />
-        <footer>
-          <Button
-            content="Next"
-            labelPosition="right"
-            icon="arrow right"
-            // onClick={() => setOpen(false)}
-            positive
-          />
-        </footer>
+      <div style={{display: 'flex', alignItems: "center", justifyContent: "center"}}>
+      <Signup/>
       </div>
     </>
   );
