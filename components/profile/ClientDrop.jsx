45394f2275f578b39f0d776d668b63eb29e762a0
Daniel Gutierrez <Dgutie299@west-mec.local>
2022-04-13 18:26:10 -0700

    profile page progress

--- components/profile/ClientDrop.jsx
+++ components/profile/ClientDrop.jsx
@@ -1,48 +1,7 @@
-// import React, { Component } from 'react'
-// import { Accordion, Icon } from 'semantic-ui-react'
-
-
-// const ClientDrop = () => {
-//   state = { activeIndex: 0 };
-
-//   handleClick = (e, titleProps) => {
-//     const { index } = titleProps;
-//     const { activeIndex } = this.state;
-//     const newIndex = activeIndex === index ? -1 : index;
-
-//     this.setState({ activeIndex: newIndex });
-//   };
-
-//   const { activeIndex } = this.state;
-
-//   return (
-//     <>
-//       <Accordion fluid styled>
-//         <Accordion.Title
-//           active={activeIndex === 2}
-//           index={2}
-//           onClick={this.handleClick}
-//         >
-//           <Icon name="dropdown" />
-//           How do you acquire a dog?
-//         </Accordion.Title>
-//         <Accordion.Content active={activeIndex === 2}>
-//           <p>
-//             Three common ways for a prospective owner to acquire a dog is from
-//             pet shops, private owners, or shelters.
-//           </p>
-//         </Accordion.Content>
-//       </Accordion>
-//     </>
-//   );
-// };
-
-// export default ClientDrop;
-
 import React, { Component } from 'react'
 import { Accordion, Icon } from 'semantic-ui-react'
 
-export default class AccordionExampleFluid extends Component {
+export default class ClientDrop extends Component {
   state = { activeIndex: 0 }
 
   handleClick = (e, titleProps) => {
