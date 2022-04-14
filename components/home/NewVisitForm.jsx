4efa54509b18a0d11b1df7c0a67c8727eef415da
jadyn <jaycal2013@gmail.com>
2022-04-14 15:02:53 -0700

    working on making the newVisit form, pulling so I can see some of Davids new work

--- components/home/NewVisitForm.jsx
+++ components/home/NewVisitForm.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import {
   Button,
   Header,
@@ -8,11 +8,9 @@ import {
   Form,
   Divider,
 } from "semantic-ui-react";
-import axios from 'axios';
+import axios from "axios";
 import { baseURL } from "../../pages/util/baseURL";
 import SearchComp from "../layout/SearchComp";
-
-
 
 //* form will ONLY collect client info. this will not create a new visit at all, just a new client
 //* the form is going to collect a name, email, address, phone number, dob, allergies, relevant medical issues  and then will have a drop down for advanced information
@@ -22,7 +20,25 @@ import SearchComp from "../layout/SearchComp";
 const NewVisitForm = () => {
   const [open, setOpen] = useState(false);
   const [clients, setClients] = useState([]);
-  const [openNewVisitForm, setOpenNewVisitForm] = useState(false)
+  const [openNewVisitForm, setOpenNewVisitForm] = useState(false);
+
+  const [newVisit, setNewVisit] = useState({
+    preferredStylist: "",
+    date: Date,
+    time: Date,
+    style: "",
+    notes: "",
+  });
+
+  //!for now it captures the clients email, so in the controller we're gonna have to use the client email to find the client from the models
+  //!this can be reworked a different way if anyone thinks of something better, this is just how i did it for now 
+
+  const { preferredStylist, date, time, style, notes } = newVisit;
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setNewVisit((prev) => ({ ...prev, [name]: value }));
+  };
 
   useEffect(() => {
     const getClients = async () => {
@@ -37,8 +53,19 @@ const NewVisitForm = () => {
   }, []);
 
   const openForm = (id) => {
-    console.log(id);
-    return id;
+    // changes the height of the div with the id that is passed through params by the onclick button
+  };
+
+  const handleSubmit = async (e) =>{
+    e.preventDefault()
+    try{
+      console.log(newVisit);
+      const res = await axios.post(`${baseURL}/api/v1/visit`, {newVisit})
+      console.log(res.data);
+      setOpen(false);
+    }catch (error){
+      console.log(error);
+    }
   }
 
   return (
@@ -73,19 +100,64 @@ const NewVisitForm = () => {
             return (
               <div class="results">
                 <>
-                  <div className="person up" id={`person ${client._id}`} onClick={() => openForm(client._id)}>
+                  <div
+                    className="person up"
+                    id={client._id}
+                    onClick={() => openForm(client._id)}
+                  >
                     <h5 className="name">{client.name}</h5>
                     <h5 className="email">{client.email}</h5>
                     <h5 className="phoneNumber">{client.phone}</h5>
                   </div>
-                  <div>
-                    
-                  </div>
+                  <Form>
+                    <Form.Field>
+                      <label>Preferred Stylist:</label>
+                      <input
+                        onChange={handleChange}
+                        name="preferredStylist"
+                        placeholder="Preferred Stylist"
+                        value={preferredStylist}
+                      />
+                    </Form.Field>
+                    <Form.Field>
+                      <label>Date:</label>
+                      <input
+                        onChange={handleChange}
+                        name="date"
+                        placeholder="Date of Appointment"
+                        value={date}
+                      />
+                    </Form.Field>
+                    <Form.Field>
+                      <label>Time:</label>
+                      <input
+                        onChange={handleChange}
+                        name="time"
+                        placeholder="Time of Appointment"
+                        value={time}
+                      />
+                    </Form.Field>
+                    <Form.Field>
+                      <label>Style:</label>
+                      <input
+                        onChange={handleChange}
+                        name="style"
+                        placeholder="Style"
+                        value={style}
+                      />
+                    </Form.Field>
+                    <Form.Field>
+                      <label>Notes:</label>
+                      <input
+                        onChange={handleChange}
+                        name="notes"
+                        placeholder="Notes"
+                        value={notes}
+                      />
+                    </Form.Field>
+                  </Form>
                   {/* we want to be able to toggle the form here */}
                   {/* put the form here, have it hidden by the height element, the onclick will edit the height of the elements whose id = client id that is passed on through onclick ( the id will be set to client._id) */}
-                  
-
-
 
                   <span className="underlined"></span>
                 </>
