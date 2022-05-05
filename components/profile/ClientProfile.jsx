4434ca5db78cad144f0ec9abe04c82b9231284d5
Daniel Gutierrez <Dgutie299@west-mec.local>
2022-05-04 18:27:32 -0700

    client page

--- components/profile/ClientProfile.jsx
+++ components/profile/ClientProfile.jsx
@@ -12,136 +12,107 @@ import {
 import VisitDrop from "./VisitDrop";
 import React, { useState, useEffect, useRef } from "react";
 
-// useEffect(() => {
-//   const asyncFunction = async () => {
-
-//     const res = await axios.get(`${baseURL}/api/v1/client`);
-//     console.log('banana', res.data.clients);
-//     setResults(res.data.clients);
-//   }
-//   asyncFunction()
-// }, [])
-
-// useEffect(() => {
-//   const getClients = async () => {
-//     try {
-//       const res = await axios.get(`${baseURL}/api/v1/client`);
-//       setClients(res.data.clients);
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   };
-//   getClients();
-// }, []);
-
-const ClientProfile = () => {
-  // {results.map((client) => {
+const ClientProfile = ({ outOfFocus, setOutOfFocus, active, setActive }) => {
   return (
-    <>
-      <div id="slideInModal" >
-        <Header>Client</Header>
 
-        <Grid columns={3} divided style={{margin: "5px"}}>
-          <Grid.Row>
-            <Grid.Column>
-              <p>Name</p>
-            </Grid.Column>
-            <Grid.Column>
-              <p>Hair Texture:</p>
-            </Grid.Column>
-            <Grid.Column>
-              <p>Medical Info:</p>
-            </Grid.Column>
-          </Grid.Row>
+    // { !active }
+    <div
+      id="slideInModal"
+      style={{ zIndex: !outOfFocus || active ? "6" : "1" }}
+    >
+      <Header>Client</Header>
 
-          <Grid.Row>
-            <Grid.Column>
-              <p>Email</p>
-            </Grid.Column>
-            <Grid.Column>
-              <p>Hair Density:</p>
-            </Grid.Column>
-            <Grid.Column>
-              <p>Allergies:</p>
-            </Grid.Column>
-          </Grid.Row>
+      <Grid columns={3} divided style={{ margin: "5px" }}>
+        <Grid.Row>
+          <Grid.Column>
+            <p>Name</p>
+          </Grid.Column>
+          <Grid.Column>
+            <p>Hair Texture:</p>
+          </Grid.Column>
+          <Grid.Column>
+            <p>Medical Info:</p>
+          </Grid.Column>
+        </Grid.Row>
 
-          <Grid.Row>
-            <Grid.Column>
-              <p>Adress</p>
-            </Grid.Column>
-            <Grid.Column>
-              <p>Hair Porosity:</p>
-            </Grid.Column>
-            <Grid.Column>
-              <p>Hair Condition:</p>
-            </Grid.Column>
-          </Grid.Row>
+        <Grid.Row>
+          <Grid.Column>
+            <p>Email</p>
+          </Grid.Column>
+          <Grid.Column>
+            <p>Hair Density:</p>
+          </Grid.Column>
+          <Grid.Column>
+            <p>Allergies:</p>
+          </Grid.Column>
+        </Grid.Row>
 
-          <Grid.Row>
-            <Grid.Column>
-              <p>Phone</p>
-            </Grid.Column>
-            <Grid.Column>
-              <p>Hair Elasticity:</p>
-            </Grid.Column>
-            <Grid.Column>
-              <p>Scalp Condition:</p>
-            </Grid.Column>
-          </Grid.Row>
+        <Grid.Row>
+          <Grid.Column>
+            <p>Adress</p>
+          </Grid.Column>
+          <Grid.Column>
+            <p>Hair Porosity:</p>
+          </Grid.Column>
+          <Grid.Column>
+            <p>Hair Condition:</p>
+          </Grid.Column>
+        </Grid.Row>
 
-          <Grid.Row>
-            <Grid.Column>
-              <p>DoB</p>
-            </Grid.Column>
-            <Grid.Column>
-              <p>Hair Length:</p>
-            </Grid.Column>
-            <Grid.Column>
-              <p>Growth Pattern:</p>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
+        <Grid.Row>
+          <Grid.Column>
+            <p>Phone</p>
+          </Grid.Column>
+          <Grid.Column>
+            <p>Hair Elasticity:</p>
+          </Grid.Column>
+          <Grid.Column>
+            <p>Scalp Condition:</p>
+          </Grid.Column>
+        </Grid.Row>
 
-        {/* <div className="form-container">
-</div> */}
+        <Grid.Row>
+          <Grid.Column>
+            <p>DoB</p>
+          </Grid.Column>
+          <Grid.Column>
+            <p>Hair Length:</p>
+          </Grid.Column>
+          <Grid.Column>
+            <p>Growth Pattern:</p>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
 
-        <Divider />
+      <Divider />
 
-        <VisitDrop />
-        <br />
-        <VisitDrop />
-        <br />
-        <VisitDrop />
+      <VisitDrop />
+      <br />
+      <VisitDrop />
+      <br />
+      <VisitDrop />
 
-        <Divider />
-        <footer>
-          <Button
-            content="Back"
-            labelPosition="left"
-            icon="arrow left"
-            onClick={() => setOutOfFocus(true)}
-            negative
-          />
-          <Button
+      <Divider />
+      <footer>
+        <Button
+          content="Back"
+          labelPosition="left"
+          icon="arrow left"
+          onClick={() => setOutOfFocus(true)}
+          negative
+        />
+        <Button
           color="green"
-            content="Edit"
-            labelPosition="edit"
-            icon="edit"
-            // onClick={() => setOutOfFocus(true)}
-            negative
-          />
-        </footer>
-      </div>
-    </>
-    //     <div class="results">
-    //       <>
-    //         <VisitFormItems client={client} setOpen={setOpen} id={client._id}  />
-    //         <span className="underlined"></span>
-    //       </>
-    //     </div>
+          content="Edit"
+          labelPosition="edit"
+          icon="edit"
+          negative
+        />
+      </footer>
+    </div>
+
+
   );
-  // })}
 };
 
 export default ClientProfile;
