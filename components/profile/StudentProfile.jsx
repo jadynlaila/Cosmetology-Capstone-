0e2e535296f1a89f1ff2c2dc716da13c63f44022
Daniel Gutierrez <Dgutie299@west-mec.local>
2022-05-09 18:27:11 -0700

    client

--- components/profile/StudentProfile.jsx
+++ components/profile/StudentProfile.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import StudentDrop from "./StudentDrop";
 import {
   Table,
@@ -13,6 +13,9 @@ import ClientDrop from "./ClientDrop";
 import ClientProfile from "./ClientProfile";
 
 const StudentProfile = () => {
+
+  const [isTeacher, setIsTeacher] = useState(false);
+  const [outOfFocus, setOutOfFocus] = useState(true);
   return (
     <>
       {/* <Segment
