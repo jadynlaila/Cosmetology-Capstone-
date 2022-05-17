6b5a8526df9c09e82c97cd47ffbb62ab36c0afc7
Daniel Gutierrez <Dgutie299@west-mec.local>
2022-05-16 18:29:26 -0700

    tried to match teaher and student

--- components/profile/TeacherProfile.jsx
+++ components/profile/TeacherProfile.jsx
@@ -168,3 +168,77 @@ const TeacherProfile = () => {
 };
 
 export default TeacherProfile;
+
+
+
+// const StudentProfile = ({user}) => {
+
+//   console.log(`student profile user`, user);
+
+//   // const [isActive, setIsActive] = useState(false);
+//   // const [outOfFocus, setOutOfFocus] = useState(true);
+//   const [fullscreen, setFullscreen] = useState(false)
+//   return (
+//     <>
+//       {/* <Segment
+//             textAlign="center"
+//             // style={{ borderTop: "solid 3rem #f3941c" }}
+//           > */}
+//       {/* <div className="header">
+//         <h2>Student Profile</h2>
+//       </div> */}
+//       <div className="profile-container">
+
+//         <div className="content">
+//           <div className="form-container">
+//             <div className="profile-header">
+//               <Image
+//                 avatar
+//                 src={"/bigChung.webp"}
+//                 alt="logo"
+//                 style={{
+//                   height: "180px",
+//                   width: "180px",
+//                   border: "solid 6px #cccddd",
+//                 }}
+//               />
+//               <h1 style={{ margin: "auto", fontSize: "4rem" }}>Student Name</h1>
+//             </div>
+//             <Divider />
+
+//             <Form>
+//               <Form.Group widths="equal" style={{ margin: "auto" }}>
+//                 <Form.Field
+//                   style={{ display: "flex", justifyContent: "center" }}
+//                   label="Email"
+//                   placeholder="Email"
+//                 />
+//                 <Form.Field
+//                   style={{ display: "flex", justifyContent: "center" }}
+//                   label="Teacher"
+//                   placeholder="Teacher"
+//                 />
+//                 <Form.Field
+//                   style={{ display: "flex", justifyContent: "center" }}
+//                   label="Hours"
+//                   placeholder="Hours"
+//                 />
+//               </Form.Group>
+//             </Form>
+
+//             <Divider />
+
+//             {/* Temporary buttons, subject to change */}
+
+
+//             {!fullscreen ?
+//               <div className="fullscreen-container">
+//                 <div className="make-fullscreen">
+//                   <Button icon="expand" floated="right" onClick={() => {
+//                     setFullscreen(!fullscreen)
+//                   }}></Button>
+//                 </div>
+
+//                 <Grid>
+
+
