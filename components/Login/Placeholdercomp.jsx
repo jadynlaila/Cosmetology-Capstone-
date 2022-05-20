f656f41447d8fa6aba5a8da91463a253710a593d
Daniel Gutierrez <Dgutie299@west-mec.local>
2022-05-19 18:27:45 -0700

    popup

--- components/Login/Placeholdercomp.jsx
+++ components/Login/Placeholdercomp.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Form, Image, Popup, Card, Rating } from "semantic-ui-react";
+import { Form, Image, Popup, Card, Rating, Button } from "semantic-ui-react";
 
 const Placeholdercomp = () => {
   return (
@@ -10,19 +10,21 @@ const Placeholdercomp = () => {
         <Form.Input>Please check your Email for your PIN</Form.Input>
       </Form> */}
 
-      <Card style={{backgroundColor: "#f2f2f2"}}>
+      <Card style={{ backgroundColor: "orange", width: "30em", margin: "auto" }}>
         <Image
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Eo_circle_green_checkmark.svg/2048px-Eo_circle_green_checkmark.svg.png"
+          src="https://media.istockphoto.com/vectors/thumb-up-winking-emoticon-vector-id1015429508"
+          // ^  Just a placeholder, thought it was funny lol
           size="small"
           style={{ marginLeft: "auto", marginRight: "auto" }}
         />
         <Card.Content>
-          <Card.Header style={{ textAlign: "center" }}>
-            Congratulations!
-          </Card.Header>
           <Card.Description>
+            Congratulations
+            <br />
             Your account has been successfully created. Please check your email
             for your PIN.
+
+            <Button>Go To Profile</Button>
           </Card.Description>
         </Card.Content>
       </Card>
