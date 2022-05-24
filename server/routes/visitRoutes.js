b42fa5a5a29edad5e748d835447ab2122c7fd338
Daniel Gutierrez <Dgutie299@west-mec.local>
2022-05-24 16:23:14 -0700

    maybe fixed it

--- server/routes/visitRoutes.js
+++ server/routes/visitRoutes.js
@@ -2,11 +2,11 @@ const router = require("express").Router();
 const {createVisit, checkIn, checkOut, getActiveVisits, getUpcomingVisits, getVisit} = require('../controllers/visit')
 
 router.route('/').post(createVisit)
-router.route("/:id").get(getVisit)
 router.route("/checkIn").put(checkIn)
 router.route('/checkOut').put(checkOut)
 router.route('/active').get(getActiveVisits)
 router.route('/upcoming').get(getUpcomingVisits)
+router.route("/:id").get(getVisit)
 
 
 module.exports = router;
