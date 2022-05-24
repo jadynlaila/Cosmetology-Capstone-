6f80fdabc25b2c8bd00675bf78dda9e17c2dc7be
David M <davidm8624@gmail.com>
2022-05-24 14:49:11 -0700

    revoery

--- pages/index.page.jsx
+++ pages/index.page.jsx
@@ -1,18 +1,17 @@
-import React, { useState } from 'react'
-import { Grid, Form, Divider } from "semantic-ui-react"
-import Head from 'next/head'
-import Image from 'next/image'
-import Navbar from '../components/layout/Navbar'
+import React, { useState } from "react";
+import { Grid, Form, Divider } from "semantic-ui-react";
+import Head from "next/head";
+import Image from "next/image";
+import Navbar from "../components/layout/Navbar";
 // import NewVisitForm from '../components/layout/NewVisitForm'
-import ActiveClients from '../components/home/ActiveClients'
-import UpcomingClients from '../components/home/UpcomingClients'
-import NewClientForm from '../components/home/NewClientForm'
-import NewVisitForm from '../components/home/NewVisitForm'
-import ClientList from '../components/home/ClientList'
-
+import ActiveClients from "../components/home/ActiveClients";
+import UpcomingClients from "../components/home/UpcomingClients";
+import NewClientForm from "../components/home/NewClientForm";
+import NewVisitForm from "../components/home/NewVisitForm";
+import ClientList from "../components/home/ClientList";
 
 const Index = ({ name }) => {
-  //! What is name? 
+  //! What is name?
   //? Is that the for the pin for clocking in?
 
   //* UseStates//
@@ -25,37 +24,31 @@ const Index = ({ name }) => {
     const { name, value, files } = e.target;
 
     if (name === "media" && files.length) {
-      setMedia(files[0])
-      setMediaPreview(() => URL.createObjectURL(files[0]))
+      setMedia(files[0]);
+      setMediaPreview(() => URL.createObjectURL(files[0]));
     } else {
       setUser((prev) => ({ ...prev, [name]: value }));
     }
-
   };
-
 
   return (
     <>
       <Navbar />
       <main className="main-container">
-
         {/* This needs to have an onClick function that renders the dropdown  */}
         <div className="radial-buttons">
-        
-          <NewClientForm open={open} setOpen={setOpen}/>
-          
-          <NewVisitForm open={open} setOpen={setOpen}/>
+          <NewClientForm open={open} setOpen={setOpen} />
+
+          <NewVisitForm open={open} setOpen={setOpen} />
           {/* This needs to have an onClick to render the form for new clients ~~~~ This is if they aren't in the system from a previous visit*/}
         </div>
-
 
         <div className="client-lists">
           <ClientList />
         </div>
-
       </main>
     </>
-  )
-}
+  );
+};
 
-export default Index
+export default Index;
